5d4a1d8b44271290402ce768fc609747be3d4a00
sanjay <sanjay8195850041@gmail.com>
2025-07-24 17:29:38 +0530

    add the components and add logo img

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,8 @@
-import QuizLogo from "../assets/quiz-logo.png"
+import logoImg from "../assets/quiz-logo.png"
 export default function Header() {
     return (
         <header>
-            <img src={QuizLogo} alt="Quiz app logo " />
+            <img src={logoImg} alt="Quiz app logo " />
             <h1>ReactQuiz</h1>
         </header>
     )
