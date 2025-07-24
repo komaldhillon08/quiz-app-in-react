823ea7ba01a1357450e055e30dd293a3a397411c
sanjay <sanjay8195850041@gmail.com>
2025-07-24 18:13:39 +0530

    add the question.js file and show question and answer in display

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-import Header from "./components/Header";
-
+import Header from "./components/Header.jsx";
+import Quiz from "./components/Quiz.jsx";
 function App() {
     return (
-        <>
+        <main>
             <Header />
-        </>
+            <Quiz />
+        </main>
     )
 }
 export default App;
--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,6 +1,33 @@
+import { useState } from "react";
+// import the question file 
+import QUESTIONS from "../questions.js"
+
 export default function Quiz() {
+
+    const [userAnswers, setUserAnswers] = useState([]);
+
+    // this is show the questopn and answers in display 
+    const activeQuestionIndex = userAnswers.length;
+
+
+    // this function using the user click the answer   
+    function handleSelectAnswer(selectAnswer) {
+        setUserAnswers((prevUserAnswer) => {
+            return [...prevUserAnswer, selectAnswer]
+        });
+    }
     return (
-        <>
-        </>
+        <div id="quiz">
+            <div id="question">
+                <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
+                <ul id="answers">
+                    {QUESTIONS[activeQuestionIndex].answers.map((answer) => (
+                        <li key={answer} className="answer">
+                            <button onClick={() => handleSelectAnswer(answer)}>{answer}</button>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        </div>
     )
 }
