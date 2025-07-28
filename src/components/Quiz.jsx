cef9006b3a04b85b9180d85321b7df287f7ab7a6
sanjay <sanjay8195850041@gmail.com>
2025-07-28 11:34:16 +0530

    add the logic if question the over then show the complete img and div

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 // import the question file 
 import QUESTIONS from "../questions.js"
+import quizCompleteImg from "../assets/quiz-complete.png"
 
 export default function Quiz() {
 
@@ -10,12 +11,25 @@ export default function Quiz() {
     const activeQuestionIndex = userAnswers.length;
 
 
+    const quizIsComplete = activeQuestionIndex === QUESTIONS.length;
+
+
     // this function using the user click the answer   
     function handleSelectAnswer(selectAnswer) {
         setUserAnswers((prevUserAnswer) => {
             return [...prevUserAnswer, selectAnswer]
         });
     }
+
+    if (quizIsComplete) {
+        return <div id="summary">
+            <img src={quizCompleteImg} alt="quiz win" />
+            <h2>Quiz Complete ....</h2>
+        </div>
+    }
+    const shuffledAnswer = [QUESTIONS[activeQuestionIndex].answers];
+    shuffledAnswer.sort(() => Math.random() - 0.5);
+
     return (
         <div id="quiz">
             <div id="question">
